74fd12568425b322bf978871d10efeddcd5a2e0f
Otavio Pinheiro <otavioaecpinheiro@gmail.com>
2023-05-13 04:52:35 -0300

    :zap: update: Atualizando para usar ConfigMap

--- shell-scrips/up/up.sh
+++ shell-scrips/up/up.sh
@@ -6,7 +6,10 @@ kind create cluster --config=k8s/kind.yaml --name=fullcycle &&
 kubectl cluster-info --context kind-fullcycle &&
 echo
 echo "Subindo deployment" &&
-kubectl apply -f k8s/deployment-variables.yaml &&
+kubectl apply -f k8s/deployment-variables-3.yaml &&
+echo
+echo "Subindo ConfigMap" &&
+kubectl apply -f k8s/configmap-env.yaml &&
 echo
 echo "Subindo service" &&
 kubectl apply -f k8s/service-clusterIP.yaml &&
